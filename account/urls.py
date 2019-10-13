f55a741b85d421d572d5b836cde437077dbeff18
rumon <rumon0016@gmail.com>
2019-10-14 01:06:58 +0600

    tempate

--- account/urls.py
+++ account/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('index/', index, name='index'),
     path('login/', login, name='login'),
     path('register/', register, name='register'),
+    path('forgot_password/', forgot_password, name='forgot_password'),
     path('logout/', logout, name='logout'),
 ]
