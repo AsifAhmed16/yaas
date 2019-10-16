e0890f55e45e0bff7abb8a591baf86b04b55a229
asif.asmed <asif.asmed@datasoft-bd.com>
2019-10-16 18:24:46 +0600

    DateTime

--- auction/forms.py
+++ auction/forms.py
@@ -6,10 +6,10 @@ class AuctionForm(forms.ModelForm):
     class Meta:
         model = Auction
         fields = '__all__'
-        exclude = ('seller', 'status', 'created_date')
+        exclude = ('seller', 'status', 'deadline', 'created_date')
         widgets = {
             "title": forms.TextInput(attrs={'class': 'form-control'}),
             "description": forms.Textarea(attrs={'class': 'form-control'}),
             "min_price": forms.NumberInput(attrs={'class': 'form-control'}),
-            "deadline": forms.DateTimeInput(attrs={'placeholder': 'dd.mm.YY HH:MM:SS', 'required': 'required'}),
+            # "deadline": forms.DateTimeInput(attrs={'placeholder': 'dd.mm.YY HH:MM:SS', 'required': 'required'}),
         }
