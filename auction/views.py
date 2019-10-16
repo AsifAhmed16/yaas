ff9ffcbfa3a8d98f9440ed208d097361fbe5fd61
rumon <rumon0016@gmail.com>
2019-10-17 02:20:00 +0600

    add auction

--- auction/views.py
+++ auction/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import *
 from django.contrib import messages
+from datetime import datetime, timedelta
 
 
 def auction_add(request):
@@ -20,15 +21,20 @@ def auction_add(request):
                     form = AuctionForm(request.POST, request.FILES or None)
                     if form.is_valid():
                         form = form.save(commit=False)
+                        form.seller = User.objects.get(id=request.session['id'])
+                        form.status = Auction_Status.objects.get(status="Active")
+                        form.deadline = datetime.now() + timedelta(days=3)
+                        form.created_date = datetime.now()
                         form.save()
                         messages.success(request, 'Auction Added')
-                        return redirect('auction:auction_add')
+                        return redirect('account:index')
                     else:
                         print(form.errors)
                         context = {
                             'data': userdata,
                             'form': form
                         }
+                        messages.error(request, 'Sorry...Something went wrong.')
                         return render(request, 'auction/auction_add.html', context)
                 return render(request, 'auction/auction_add.html', context)
             else:
